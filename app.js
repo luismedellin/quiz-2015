5cf3c3478d07ca3b265418a01d8b736f39616ff8
Luis Velandia <luiseduardo1218@gmail.com>
2015-06-29 15:05:32 -0500

    express-partials

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express =   require('express');
+var path =      require('path');
+var favicon =   require('serve-favicon');
+var logger =    require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials=   require('express-partials');
 
-var routes = require('./routes/index');
+var routes =    require('./routes/index');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
