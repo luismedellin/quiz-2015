e8d27d1ec4f4173980bb625255eabb26f1e8818e
Luis Velandia <luiseduardo1218@gmail.com>
2015-07-05 16:59:51 -0500

    Lista de Preguntas

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 //console.log('DEBUG:'+quiz_controller);
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes', 						quizController.index);
+router.get('/quizes/:quizId(\\d)',			quizController.show);
+router.get('/quizes/:quizId(\\d)/answer', 	quizController.answer);
 
 router.get('/author', function (req, res) {
 	res.render('author.ejs');
