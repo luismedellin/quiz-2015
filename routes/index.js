f91c295a4e8d2f1322f80a9d9c5ee172c1836743
Luis Velandia <luiseduardo1218@gmail.com>
2015-07-26 22:27:00 -0500

    Editar pregunta

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,12 @@ var quizController = require('../controllers/quiz_controller');
 router.param('quizId', quizController.load); //autoload :quizId
 
 router.get('/quizes', 						quizController.index);
-router.get('/quizes/:quizId(\\d)',			quizController.show);
-router.get('/quizes/:quizId(\\d)/answer', 	quizController.answer);
+router.get('/quizes/:quizId(\\d+)',			quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
 router.get('/quizes/new',					quizController.new);
 router.post('/quizes/create',				quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', 	quizController.edit);
+router.put('/quizes/:quizId(\\d+)', 		quizController.update);
 
 router.get('/author', function (req, res) {
 	res.render('author.ejs');
