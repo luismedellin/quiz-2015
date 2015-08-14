8201f4b332fac89f3c2b53fe4167571844fc454c
Luis Velandia <luiseduardo1218@gmail.com>
2015-08-13 21:03:11 -0500

    Estadisticas finalizadas

--- controllers/statistic_controller.js
+++ controllers/statistic_controller.js
@@ -1,6 +1,7 @@
 var models = require('../models/models.js');
 
 //GET /quizes/statistics
+//Consulta las estadisticas de la aplicación
 exports.show = function (req, res) {
 	var data = {
 		nPreguntas: 0, 
@@ -11,17 +12,17 @@ exports.show = function (req, res) {
 	};
 
 //El número de preguntas
-models.sequelize.query('SELECT COUNT(id) AS cantidad FROM Quizzes;').then(function (cantidad) {
+models.sequelize.query('SELECT COUNT(id) AS cantidad FROM "Quizzes";').then(function (cantidad) {
 	data.nPreguntas=cantidad[0][0].cantidad;
 	//El número de comentarios totales
-	models.sequelize.query('SELECT COUNT(id) AS cantidad FROM Comments;').then(function (cantidad) {
+	models.sequelize.query('SELECT COUNT(id) AS cantidad FROM "Comments";').then(function (cantidad) {
 		data.nComentarios=cantidad[0][0].cantidad;
 		//si hay preguntas
 		if(data.nPreguntas!==0){
 			data.nPromComent=data.nComentarios/data.nPreguntas;
 		}
 		//El número de preguntas sin comentarios
-		models.sequelize.query('SELECT 	COUNT(A.id) AS cantidad FROM Quizzes A LEFT JOIN Comments B ON A.id=B.QuizId WHERE 	B.QuizId IS NULL;').then(function (cantidad) {
+		models.sequelize.query('SELECT count(*) AS cantidad FROM "Quizzes" WHERE "id" NOT IN (SELECT DISTINCT "QuizId" FROM "Comments")').then(function (cantidad) {
 			data.nPregSinComen=cantidad[0][0].cantidad;
 			//El número de preguntas con comentarios
 			data.nPregConComen=data.nPreguntas-data.nPregSinComen;
